d7569f79217dbed68539f1e6470ceba9e501d78e
tshkreddy91 <sreeharishmail@gmail.com>
2025-05-17 21:58:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 from datetime import datetime
 import pytz
 from streamlit_autorefresh import st_autorefresh
-
+st.set_page_config(layout="wide")
 # Constants
 BINANCE_API_URL = "https://fapi.binance.com"
 SYMBOLS_ENDPOINT = "/fapi/v1/exchangeInfo"
